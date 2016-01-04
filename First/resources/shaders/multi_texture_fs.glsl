13b339d60bf00087084bf6301e46e11004c562f8
elloop <elloop@foxmail.com>
2016-01-04 21:46:32 +0800

    remove unused dll

--- First/resources/shaders/multi_texture_fs.glsl
+++ First/resources/shaders/multi_texture_fs.glsl
@@ -3,15 +3,14 @@ precision lowp float;
 varying     vec2        outUv_;
 uniform     sampler2D   textureBg_;
 uniform     sampler2D   textureCloud_;
-uniform     float       uvAnim_;
+uniform     float       deltaUv_;
 
 
 void main() 
 {
     vec4    bgColor    = texture2D(textureBg_, outUv_);
-    vec2    newUv      = vec2(outUv_.x + uvAnim_, outUv_.y);
-    vec4    cloudColor = texture2D(textureCloud_, newUv);
-    gl_FragColor       = bgColor + cloudColor;
-    /* gl_FragColor       = bgColor * cloudColor; */
+    vec2    moveUv     = vec2(outUv_.x + deltaUv_, outUv_.y);
+    vec4    cloudColor = texture2D(textureCloud_, moveUv);
+    gl_FragColor    = bgColor + cloudColor;
     /* gl_FragColor       = mix(bgColor, cloudColor, 0.5); */
 }
